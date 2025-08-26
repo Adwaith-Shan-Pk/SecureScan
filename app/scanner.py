e7daac05ff2985a629240b783ac58559e7d71929
Adwaith-Shan-Pk <pkadwaithshan@gmail.com>
2025-08-27 00:07:51 +0530

    Setup For Local Testing And Adding req file

--- app/scanner.py
+++ app/scanner.py
@@ -1,7 +1,8 @@
 import clamd
-
+#VM-IP = 'http://192.168.1.9'
 def scan_file(filepath):
-    cd = clamd.ClamdUnixSocket() # Connect to ClamAV daemon via Unix socket caz daemon already running
+    #cd = clamd.ClamdUnixSocket() # Use this if clamd is set up with a Unix socket
+    cd = clamd.ClamdNetworkSocket(host='192.168.1.9', port=3310, timeout=60) # Use this for network socket
 
     try:
         result = cd.scan(filepath)
